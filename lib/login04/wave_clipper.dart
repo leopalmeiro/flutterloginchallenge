57f1793568aa6073bc858a47326baf7f9cf9354f
leopalmeiro <leonardo.palmeiro@babel.es>
2020-06-17 22:27:53 +0200

    add waves

--- lib/login04/wave_clipper.dart
+++ lib/login04/wave_clipper.dart
@@ -4,11 +4,20 @@ class WavePathClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     final path = Path();
-    path.lineTo(0.0, size.height);
-    path.lineTo(10, size.height - 10);
-    path.lineTo(20, size.height);
-    path.lineTo(size.width, size.height);
+    final heightBreak = size.height * 0.90;
+    final totalCurves = 2;
+    final amplitudUp = heightBreak * 0.90;
+    final amplitudDown = size.height;
+    final finalXpoin = size.width / totalCurves;
+    final points = {'x': 0.0, 'y': heightBreak};
 
+
+    path.lineTo(0.0, heightBreak);
+    path.cubicTo(points['x'], points['y'], finalXpoin / 2, amplitudUp,
+        finalXpoin, heightBreak);
+
+    path.cubicTo(finalXpoin, heightBreak, size.width * 0.75, amplitudDown,
+        size.width, heightBreak);
     path.lineTo(size.width, 0.0);
 
     path.close();
